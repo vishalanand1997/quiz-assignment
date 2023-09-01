59b6bd785a42b262f5819ce23c5122e766917666
Vishal Anand <anandvishal888@gmail.com>
2023-09-02 02:54:24 +0530

    dummy commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <Fragment>
       <Head>
-        <title>Login with email</title>
+        <title>Login with email </title>
         <meta name="description" content="login with email address" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
